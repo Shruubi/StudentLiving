1ba22dc0d3d1d1a55779e464fb1c0171204a0fe7
Damon Swayn <dswayn@email.com>
2014-08-16 16:36:07 +1000

    Initial Commit

---
+++ StudentLiving/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StudentLiving
+//
+//  Created by Damon Swayn on 16/08/2014.
+//  Copyright (c) 2014 Swayn Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
